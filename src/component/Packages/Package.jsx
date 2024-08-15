8d1243a3d0479844375923e077f605ddac777671
luqmanofficial <lk3281751@gmail.com>
2024-08-15 11:08:12 +0500

    price Change

--- src/component/Packages/Package.jsx
+++ src/component/Packages/Package.jsx
@@ -100,7 +100,7 @@ function Package() {
             <p className="text-2xl text-[#18003c] mb-3 font-bold">Annual Membership</p>
             <p className="font-bold mb-4 text-pink-600 text-xl"><span className='text-gray-500 line-through'>Rs 2500</span> <span className='underline'>Rs 1,460</span></p>
             <button className="bg-[#18003c] mb-3 text-white py-2 px-4 rounded hover:bg-pink-600 transition-transform duration-300 ease-in-out transform hover:scale-105  hover:border-light-blue-300 focus:outline-none focus:ring-2 focus:ring-light-blue-500 focus:ring-opacity-50"
-              onClick={() => addToCart('Annual Membership', 2500, asset31)}
+              onClick={() => addToCart('Annual Membership', 1,460, asset31)}
             >
               Purchase
             </button>
